e12807bbdbc35c2bb83611e12ec69b8f53e3be3d
Utsav <justiceleaguefeb97@gmail.com>
2023-03-05 01:32:33 +0530

    second commmit

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((result) => {
     console.log("DATABASE CONNECTED !");
     app.listen(3000, () => console.log("SERVER RUNNING AT PORT 3000 !"));
