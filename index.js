79b9066d5686db1ce073701b3df8ed424386b4a1
Utsav <justiceleaguefeb97@gmail.com>
2023-03-05 01:21:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = require("./routes.js");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then((result) => {
+    console.log("DATABASE CONNECTED !");
+    app.listen(3000, () => console.log("SERVER RUNNING AT PORT 3000 !"));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
