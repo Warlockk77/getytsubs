fac1cfbc1d67b8126f749e1ddf60f8eb9e544763
Utsav <justiceleaguefeb97@gmail.com>
2023-03-06 11:10:49 +0530

    final

--- tests/subscribers.test.js
+++ tests/subscribers.test.js
@@ -13,7 +13,7 @@ afterEach(async () => {
 });
 
 describe("GET /", () => {
-  it("should return index.html ", async () => {
+  it("SHOULD BE RETR]URNING INDEX.HMTL ", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.header["content-type"]).toBe("text/html; charset=UTF-8");
@@ -21,24 +21,16 @@ describe("GET /", () => {
 });
 
 describe("GET /subscribers", () => {
-  it("should return an array of subscribers ", async () => {
+  it("SHOULD RETURN ARRAY OF SUBS ", async () => {
     const res = await request(app).get("/subscribers");
     expect(res.statusCode).toBe(200);
   });
 });
 
 describe("GET /subscribers/names", () => {
-  it("should return an array of subscribers name and subscribedChannel ", async () => {
+  it("SHOULD RETURN SUBS AND SUBSCRIBED CHANNEL NAMES ", async () => {
     const res = await request(app).get("/subscribers/names");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBeGreaterThan(0);
-  });
-});
-
-describe("GET /subscribers/:id", () => {
-  it("should return an array of subscribers name and subscribedChannel ", async () => {
-    const res = await request(app).get("/subscribers/63ef0ee6cd4ba7a97e047bd5");
-    expect(res.statusCode).toBe(200);
-    expect(res.body._id).toBe("63ef0ee6cd4ba7a97e047bd5");
   });
 });
